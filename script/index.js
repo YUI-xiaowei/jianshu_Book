0835e1f3b526d49a92b0380b8d9dc0df1873dfb3
钟洁怡 <1016396296@qq.com>
2016-03-19 22:09:36 +0800

    book

---
+++ script/index.js
@@ -0,0 +1,21 @@
+// JavaScript Document
+//图标2
+function mychange1()
+{ document.img2.src="images/2.png";}
+function myshow1()
+{ document.img2.src="images/3.png";}
+//图标3
+function mychange2()
+{ document.img3.src="images/4.png";}
+function myshow2()
+{ document.img3.src="images/9.png";}
+//图标4
+function mychange3()
+{ document.img4.src="images/7.png";}
+function myshow3()
+{ document.img4.src="images/5.png";}
+//图标5
+function mychange4()
+{ document.img5.src="images/8.png";}
+function myshow4()
+{ document.img5.src="images/6.png";}
